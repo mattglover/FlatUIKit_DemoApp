b189c44f7d4fdd653c6938151d46fe26fd623154
Matt Glover <matt@duchysoftware.com>
2013-05-24 22:40:46 +0100

    Initial Commit

---
+++ FlatUIKit_DEMO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlatUIKit_DEMO
+//
+//  Created by Matt Glover on 24/05/2013.
+//  Copyright (c) 2013 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
